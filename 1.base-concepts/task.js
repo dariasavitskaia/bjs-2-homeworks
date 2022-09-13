6ef79b667a8c9481aa66a2e0c598b1b4c690325c
dariasavitskaia <libtech1991@gmail.com>
2022-09-13 21:54:56 +0300

    homework done

--- 1.base-concepts/task.js
+++ 1.base-concepts/task.js
@@ -1,7 +1,18 @@
+"use strict";
+
 function solveEquation(a, b, c) {
+  const d = Math.pow(b, 2) - 4 * a * c;
   let arr;
+  if (d < 0) {
+    arr = [];
+  } else if (d == 0) {
+    arr = [-b / (2 * a)];
+  } else {
+    arr = [(-b + Math.sqrt(d)) / (2 * a), (-b - Math.sqrt(d)) / (2 * a)];
+  }
+
   // код для задачи №1 писать здесь
-  console.log("test1");
+
   return arr; // array
 }
 
