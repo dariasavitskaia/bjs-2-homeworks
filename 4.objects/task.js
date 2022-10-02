7869a1c39b3f3b5c6c4c4253839eda2fd8b35b6a
dariasavitskaia <libtech1991@gmail.com>
2022-10-03 00:53:38 +0300

    object_solution

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,10 +1,50 @@
 function Student(name, gender, age) {
-    // Ваш код
+  this.name = name;
+  this.gender = gender;
+  this.age = age;
 
+  // Ваш код
 }
 
 Student.prototype.setSubject = function (subjectName) {
+  this.subject = subjectName;
   //ваш код
-}
+};
 
-// ваш код для остальных методов
+Student.prototype.addMark = function (mark) {
+  if (this.marks === undefined) {
+    this.marks = [mark]; // добавить первую оценку
+  } else {
+    this.marks.push(mark); // добавить вторую и последующие оценки в массив
+  }
+};
+
+Student.prototype.addMarks = function (...marks) {
+  if (this.marks === undefined) {
+    this.marks = marks;
+  } else {
+    this.marks.push(marks);
+  }
+};
+
+Student.prototype.getAverage = function () {
+  if (this.marks === undefined) {
+    return "No marks to be defined as average";
+  } else {
+    const averageMark =
+      this.marks.reduce((a, b) => a + b, 0) / this.marks.length;
+    return averageMark;
+  }
+};
+
+Student.prototype.exclude = function (reason) {
+  if (this.marks !== undefined && this.subject !== undefined) {
+    delete this.mark;
+    delete this.subject;
+    this.excluded = reason;
+  }
+};
+// student1.getAverage();
+
+// console.log(student1.getAverage());
+// //console.log(student1);
