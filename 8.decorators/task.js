6581b7ace6f13befeba1627798be2991124b5167
dariasavitskaia <libtech1991@gmail.com>
2022-11-27 00:29:07 +0300

    renew

--- 8.decorators/task.js
+++ 8.decorators/task.js
@@ -4,48 +4,43 @@ function cachingDecoratorNew(func) {
   function wrapper(...args) {
     const hash = args.join(","); // получаем правильный хэш
     let objectInCache = cache.find((item) => item.hash == hash); // ищем элемент, хэш которого равен нашему хэшу
-    if (!objectInCache) {
-      // если элемент не найден
-      let result = func(...args); // в кэше результата нет - придётся считать
-      cache.push({ hash: hash, value: result }); // добавляем элемент с правильной структурой
+    if (objectInCache) {
+      // если элемент найден
 
-      if (cache.length > 5) {
-        cache.shift(); // если слишком много элементов в кэше надо удалить самый старый (первый)
-      }
-      console.log("Вычисляем: " + result);
-      return "Вычисляем: " + result;
+      console.log("Из кэша: " + objectInCache.value); // индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
+      return "Из кэша: " + objectInCache.value;
     }
 
-    console.log("Из кэша: " + cache.find((item) => item.hash == hash).value); // индекс нам известен, по индексу в массиве лежит объект, как получить нужное значение?
-    return "Из кэша: " + cache.find((item) => item.hash == hash).value;
+    let result = func(...args); // в кэше результата нет - придётся считать
+    cache.push({ hash: hash, value: result }); // добавляем элемент с правильной структурой
+    if (cache.length > 5) {
+      cache.shift(); // если слишком много элементов в кэше надо удалить самый старый (первый)
+    }
+    console.log("Вычисляем: " + result);
+    return "Вычисляем: " + result;
   }
   return wrapper;
 }
 
-function debounceDecorator(func, delay) {
+function debounceDecoratorNew(func, delay) {
   let timeoutId = null;
   wrapper.count = 0;
   wrapper.allCount = 0;
-
   function wrapper(...args) {
     if (wrapper.allCount == 0) {
-      delay_adj = 0;
-    } else {
-      delay_adj = delay;
-    }
-
-    wrapper.allCount += 1;
-    if (timeoutId) {
-      //console.log("удали текущий таймаут");
-      clearTimeout(timeoutId);
-    }
-    //console.log("создаем новый таймаут");
-    timeoutId = setTimeout(() => {
-      timeoutId = null;
       func(...args);
       wrapper.count += 1;
-      //console.log("вызвали колбек");
-    }, delay_adj);
+    }
+    wrapper.allCount += 1;
+
+    if (timeoutId) {
+      clearTimeout(timeoutId);
+    }
+
+    timeoutId = setTimeout(() => {
+      func(...args);
+      wrapper.count += 1;
+    }, delay);
   }
   return wrapper;
 }
